98d05ec006f6519842148e88bf32315cacd96a1e
snekw <dev.snekw@gmail.com>
2021-05-05 12:50:41 +0300

    LCJS 3.0.0

--- src/index.js
+++ src/index.js
@@ -31,14 +31,11 @@ const slice = gauge
     .getDefaultSlice()
     .setInterval(0, 400000)
     .setValue(329000)
-    .setFillStyle(new SolidFill({ color: ColorRGBA(12, 213, 87) }))
+    // .setFillStyle(new SolidFill({ color: ColorRGBA(12, 213, 87) }))
     .setName('2019 sales')
 
 // Add LegendBox and define the position in the chart
 const legend = gauge.addLegendBox()
-    .setPosition({ x: 0, y: 0 })
-    .setOrigin(UIOrigins.LeftBottom)
-    .setMargin({ top: 5, right: 5, bottom: 5, left: 5 })
 
 // Add gaugeChart to LegendBox
 legend.add(gauge)
